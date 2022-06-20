7d1e3febf2922716457c2bb76447091db2590bda
Kaan <kaangogcay@gmail.com>
2022-06-20 10:04:18 +0200

    fix inventory

--- src/main/java/groep3/test/entities/Category.java
+++ src/main/java/groep3/test/entities/Category.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     public Long ID;
 
